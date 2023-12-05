ede0da7a1cdd2bcd923718a42001f330f563e09d
Nathan Lock <nathan.lock@coderus.com>
2023-12-05 18:47:52 +0000

    feat: implement part1 and 2 of scratchcard coding challenge

--- scratchcards-adventofcode4-net/Program.cs
+++ scratchcards-adventofcode4-net/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+
+        private const string _filePath = "input.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var input = File.ReadAllText(_filePath);
+            var cards = ScratchcardHelper.ParseCardsString(input);
+            var matchesPerCard = ScratchcardHelper.CountMatches(cards);
+            // Part 1
+            var totalPoints = ScratchcardHelper.CalculateTotalPoints(matchesPerCard); 
+            Console.WriteLine($"Total number of points: {totalPoints}");
+            // Part 2
+            var totalTickets = ScratchcardHelper.CalculateTotalTickets(matchesPerCard);
+            Console.WriteLine($"Total number of tickets: {totalTickets}");
         }
     }
 }
